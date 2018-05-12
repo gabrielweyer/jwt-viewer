eb32f7157451997ba85eedc45c8590413e25a25c
Gabriel Weyer <gabrielweyer@gmail.com>
2018-05-12 10:01:45 +1000

    Add Base64 decode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,22 @@ import { JwtService, Jwt } from '../shared/jwt.service';
 })
 export class AppComponent {
   jwt: Jwt;
+  decodedBase64: string;
 
   constructor(private readonly clippy: ClippyService, private readonly jwtService: JwtService) {}
 
-  paste(ev: ClipboardEvent): void {
+  pasteJwt(ev: ClipboardEvent): void {
     this.jwt = null;
+    this.decodedBase64 = null;
+
     const value = this.clippy.getClipboard(ev);
     this.jwt = this.jwtService.getJwt(value);
+  }
+
+  pasteBase64(ev: ClipboardEvent): void {
+    this.decodedBase64 = null;
+
+    const value = this.clippy.getClipboard(ev);
+    this.decodedBase64 = atob(value);
   }
 }
