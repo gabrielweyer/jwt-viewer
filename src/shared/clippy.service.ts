b48f70553d5c56658bfdf5a4c26ee71655a050e7
Gabriel Weyer <gabriel.weyer@readify.net>
2019-12-25 16:47:19 +1100

    Fix linting errors

--- src/shared/clippy.service.ts
+++ src/shared/clippy.service.ts
@@ -2,8 +2,8 @@ export class ClippyService {
   getClipboard(ev: ClipboardEvent): string {
     if (ev.clipboardData) {
       return ev.clipboardData.getData('text/plain');
-    } else if (window['clipboardData']) {
-      return window['clipboardData'].getData('Text');
+    } else if (window[`clipboardData`]) {
+      return window[`clipboardData`].getData('Text');
     }
   }
 }
